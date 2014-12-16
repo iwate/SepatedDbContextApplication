5fa227cef6949b1f3de5d0221556aaadfc26c6b4
Yoshiyuki Taniguchi <iwatesoftware@live.jp>
2014-12-16 17:54:32 +0900

    create web app

---
+++ SepatedDbContextApplication.Web/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SepatedDbContextApplication.DataAccess;
+using SepatedDbContextApplication.Entity;
+using System;
+
+namespace SepatedDbContextApplication.Web
+{
+	public partial class Startup
+	{
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(IdentityContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, User>(
+                        validateInterval: TimeSpan.FromMinutes(20),
+                        regenerateIdentity: (manager, user) => manager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie))
+                }
+            });
+        }
+	}
+}
---
+++ SepatedDbContextApplication.Web/App_Start/Startup.Data.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+using SepatedDbContextApplication.DataAccess;
+
+namespace SepatedDbContextApplication.Web
+{
+    public partial class Startup
+    {
+        public void ConfigureData(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(DataContext.Create);
+        }
+    }
+}
