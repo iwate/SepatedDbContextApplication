747ef36abd418fde80bc27914c0bcf9876de89bd
Yoshiyuki Taniguchi <iwatesoftware@live.jp>
2014-12-16 17:37:26 +0900

    Revert "Revert "create data accessor""
    This reverts commit db12cd099c00c4a1f6db1c647872d95eb723e719.

---
+++ SepatedDbContextApplication.DataAccess/DataRepository.cs
@@ -0,0 +1,45 @@
+﻿using SepatedDbContextApplication.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SepatedDbContextApplication.DataAccess
+{
+    public class DataRepository : IDataRepository
+    {
+        private DataContext Db;
+        public DataRepository(DataContext db)
+        {
+            Db = db;
+        }
+        public async Task<Datum> Find(int id)
+        {
+            return await Db.Data.FindAsync(id);
+        }
+
+        public IEnumerable<Datum> GetAll()
+        {
+            return Db.Data.ToList();
+        }
+
+        public void Add(Datum datum)
+        {
+            Db.Data.Add(datum);
+        }
+
+        public void Remove(Datum datum)
+        {
+            Db.Data.Remove(datum);
+        }
+
+        public async Task Save()
+        {
+            await Db.SaveChangesAsync();
+        }
+        public async Task<bool> IsConflict(Datum datum)
+        {
+            return await Db.Data.FindAsync(datum.Id) != null;
+        }
+    }
+}
